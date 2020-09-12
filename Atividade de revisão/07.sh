4bbd35638a62979bc1e7de40284caba9228cacb2
Palitot <71037376+Palitot@users.noreply.github.com>
2020-09-12 18:00:36 -0300

    Update 07.sh

--- Atividade de revisão/07.sh
+++ Atividade de revisão/07.sh
@@ -5,5 +5,5 @@ B="$(ls -l $2)"
 C="$(ls -l $3)"  
 
 echo -e "Informações de $1:" ${A} >> info.txt  
-echo -e"Informações de $2:" ${B} >> info.txt 
-echo -e"Informações de $3:" ${C} >> info.txt 
+echo -e "Informações de $2:" ${B} >> info.txt 
+echo -e "Informações de $3:" ${C} >> info.txt 
