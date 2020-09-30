1010c9f2b23d12d5faaf8475f442a8451e206bb6
Palitot <71037376+Palitot@users.noreply.github.com>
2020-09-30 10:43:10 -0300

    Update 6.sh

--- Atividade 03/6.sh
+++ Atividade 03/6.sh
@@ -5,6 +5,6 @@ arq1=$(cat $1 | grep ${proibido})
 arq2=$(cat $2 | grep ${proibido})
 arq3=$(cat $3 | grep ${proibido})
 
-test "${arq1}" == "${proibido}" && echo "$1" && mv $1 /tmp || echo "Arquivo 1 tá limpo"
-test "${arq2}" == "${proibido}" && echo "$2" && mv $2 /tmp || echo "Arquivo 2 tá limpo"
-test "${arq3}" == "${proibido}" && echo "$3" && mv $3 /tmp || echo "Arquivo 3 tá limpo"
+test "${arq1}" == "${proibido}" && echo $1 && mv $1 /tmp || echo "Arquivo 1 tá limpo"
+test "${arq2}" == "${proibido}" && echo $2 && mv $2 /tmp || echo "Arquivo 2 tá limpo"
+test "${arq3}" == "${proibido}" && echo $3 && mv $3 /tmp || echo "Arquivo 3 tá limpo"
