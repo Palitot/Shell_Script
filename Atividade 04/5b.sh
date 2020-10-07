27451a9ff9473784470c87fbc4a83325bdc191b7
Palitot <71037376+Palitot@users.noreply.github.com>
2020-10-07 08:31:57 -0300

    Update 5b.sh

--- Atividade 04/5b.sh
+++ Atividade 04/5b.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 while true; do
 	a=$(( ${RANDOM} % 10 ))
-	touch ${a} 2>> 2.log && echo “$(date +%H:%M) ${a}: criado!” >> 1.log
+	touch ${a} 2>> 3.log && echo “$(date +%H:%M) ${a}: criado!” >> 3.log
 	sleep 1
 b=$(( ${RANDOM} % 10 ))
-	rm ${b} 2>> 2.log && echo “$(date +%H:%M) ${b}: removido!” >> 1.log
-	cat 1.log >> 3.log | cat 2.log >> 3.log
+	rm ${b} 2>> 3.log && echo “$(date +%H:%M) ${b}: removido!” >> 3.log
 done
